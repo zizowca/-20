feb3c5358814ce9156cad38c1dacd11c4805d65c
Азиза Амануллина <zizowca@gmail.com>
2022-01-06 16:38:58 +0300

    Добавьте файлы проекта.

---
+++ Задание 20/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_20
+{
+    class Program
+    {
+        delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+            Console.Write("Введите r:\t");
+            double r = Convert.ToDouble(Console.ReadLine());
+            MyDelegate length = Length;
+            if (length!=null)
+            {
+                double l = length(r);
+                Console.WriteLine(l);
+            }
+            MyDelegate area = Area;
+            if (length != null)
+            {
+                double s = area(r);
+                Console.WriteLine(s);
+            }
+            MyDelegate vol = Vol;
+            if (length != null)
+            {
+                double v = vol(r);
+                Console.WriteLine(v);
+            }
+            Console.ReadKey();
+        }
+        static double Length(double r)
+        {
+            Console.Write("Длина окружности с укзанным радиусом =\t");
+            return 2 * Math.PI * r;
+        }
+        static double Area(double r)
+        {
+            Console.Write("Площадь круга с укзанным радиусом =\t");
+            return Math.PI * Math.Pow(r, 2);
+        }
+        static double Vol(double r)
+        {
+            Console.Write("Объем шара с укзанным радиусом =\t");
+            return 4 / 3 * Math.PI * Math.Pow(r, 3);
+        }
+    }
+}
---
+++ Задание 20/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Задание 20")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Задание 20")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("22ea2274-7bca-4e77-831d-8fe03652f812")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
